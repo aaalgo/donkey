6481c54203dd5b8b3e68f6277910a945ed8d5f7e
Wei Dong <wei.dong@iailabs.com>
2019-01-31 00:12:44 -0500

    update

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,10 @@ import os
 import subprocess as sp
 from distutils.core import setup, Extension
 
+if not os.path.exists('kgraph'):
+    sp.check_call('git clone https://github.com/aaalgo/kgraph', shell=True)
+    pass
+
 try:
     CONFIG_DIR = os.environ['CONFIG_DIR']
 except:
@@ -22,16 +26,16 @@ else:
         boost_python = 'boost_python%d%d' % (sys.version_info[0], sys.version_info[1])
     pass
 
-libraries.extend(['kgraph', 'boost_timer', 'boost_chrono', 'boost_log_setup', 'boost_log', 'boost_filesystem', 'boost_system', 'boost_container', boost_python])
+libraries.extend(['boost_timer', 'boost_chrono', 'boost_log_setup', 'boost_log', 'boost_filesystem', 'boost_system', 'boost_container', boost_python])
 
 donkey = Extension('donkey',
         language = 'c++',
         extra_compile_args = ['-O3', '-std=c++1y', '-g', '-frtti', '-DBOOST_LOG_DYN_LINK=1'], #, '-DBOOST_ERROR_CODE_HEADER_ONLY'], 
-		include_dirs = ['/usr/local/include', CONFIG_DIR, '.'],
+		include_dirs = ['/usr/local/include', CONFIG_DIR, '.', './kgraph'],
         libraries = libraries,
         library_dirs = ['/usr/local/lib'],
 
-        sources = ['python-api.cpp', 'donkey.cpp', 'logging.cpp', 'index-kgraph.cpp', 'index-lsh.cpp', 'kgraph_lite.cpp', 'fixed_monotonic_buffer_resource.cpp'],
+        sources = ['python-api.cpp', 'donkey.cpp', 'logging.cpp', 'index-kgraph.cpp', 'index-lsh.cpp', 'kgraph_lite.cpp', 'fixed_monotonic_buffer_resource.cpp', 'kgraph/kgraph.cpp', 'kgraph/metric.cpp'],
         undef_macros = [ "NDEBUG" ]
         )
 
